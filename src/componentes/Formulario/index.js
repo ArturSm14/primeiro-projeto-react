972b7b49e571c1be42f3c9284fff45611994538b
ArturSm14 <antonioartur123456@gmail.com>
2024-02-01 23:35:59 -0300

    finalizando projeto

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,34 +1,62 @@
+import { useState } from 'react'
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import DropDown from '../DropDown'
 import './Formulario.css'
 
 
-const Formulario = () => {
+const Formulario = (props) => {
 
-    const times = [
-        'Valorant',
-        'League of Legengs',
-        'Overwatch',
-        'Counter-Strike',
-        'Apex Legends',
-        'PUBG',
-        'Wild-Rift'
-    ]
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido')
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
             <h2>Preencha os dados par criar o card do colaborador</h2>
-            <CampoTexto obrigatorio={true} label="Nome" placeholder = "Digite seu nome"/>
-            <CampoTexto obrigatorio={true} label="Cargo"placeholder = "Digite seu cargo"/>
-            <CampoTexto label="Imagem" placeholder = "Digite o endereço da imagem"/>
-            <DropDown obrigatorio={true} label="Time" itens= {times}/>
+            <CampoTexto 
+                obrigatorio={true} 
+                label="Nome" 
+                placeholder = "Digite seu nome"
+                valor = {nome}
+                aoAlterado = {valor => setNome(valor)}
+            />
+            <CampoTexto
+                obrigatorio={true} 
+                label="Cargo"
+                placeholder = "Digite seu cargo"
+                valor = {cargo}
+                aoAlterado = {valor => setCargo(valor)}
+            />
+            <CampoTexto 
+                label="Imagem" 
+                placeholder = "Digite o endereço da imagem"
+                valor = {imagem}
+                aoAlterado = {valor => setImagem(valor)}
+            />
+            <DropDown 
+            obrigatorio={true} 
+            label="Time" 
+            itens= {props.times}
+            valor = {time}
+            aoAlterado = {valor => setTime(valor)}
+            />
             <Botao>
                 Criar Card
             </Botao>
